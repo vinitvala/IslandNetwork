b1df12924b3d041baddeddb97e88b19054ef1d37
Vinit Vala <70471601+vinitvala@users.noreply.github.com>
2024-07-20 03:04:55 -0400

    Add files via upload

---
+++ src/NoRouteException.java
@@ -0,0 +1,16 @@
+/**
+ * @author Vinit Vala
+ * SBU ID: 114501080
+ * Recitation 2
+ * Node Class.
+ */
+
+/**
+ * Exception class for no route
+ */
+public class NoRouteException extends Exception{
+    public NoRouteException(String message) {
+        super(message);
+    }
+}
+
